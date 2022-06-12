dfcce1bc0f86f7020821daed2b2862350595d78c
Fahim Al Islam <fahim6668@gmail.com>
2022-06-12 18:47:26 +0600

    Updated: edited admin title

--- auction_core/settings/admin_settings.py
+++ auction_core/settings/admin_settings.py
@@ -1,11 +1,13 @@
+from django.utils.timezone import now
+
 BATON = {
     'SITE_HEADER': 'Digital Auction',
     'SITE_TITLE': 'Digital Auction',
     'INDEX_TITLE': 'System Admin',
     'SUPPORT_HREF': 'https://support.university-auction.marvelous-tech.com',
     'COPYRIGHT':
-        'Copyright © 2020 '
-        '<a href="https://university-auction.marvelous-tech.com">Digital Auction</a>',
+        f'Copyright © 2020 {now().year} '
+        '<a href="https://university-auction.marvelous-tech.com">Marvelous Technologies</a>',
     'POWERED_BY': '<a href="https://www.marvelous-tech.com">Marvelous Technologies</a>',
     'CONFIRM_UNSAVED_CHANGES': True,
     'SHOW_MULTIPART_UPLOADING': True,
@@ -15,6 +17,6 @@ BATON = {
     'CHANGELIST_FILTERS_FORM': True,
     'MENU_ALWAYS_COLLAPSED': False,
     'MENU_TITLE': 'Menu',
-    'MESSAGES_TOASTS': False,
+    'MESSAGES_TOASTS': True,
     'GRAVATAR_DEFAULT_IMG': 'retro',
 }
